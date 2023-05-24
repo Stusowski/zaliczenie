bc72a666cc178232b54f1c732f35ed92c5d8aed2
Stusowski <126696159+Stusowski@users.noreply.github.com>
2023-05-24 14:47:27 +0000

    Program sumujący elementy macierzy

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hjhello world!");
+    Scanner in = new Scanner(System.in);
+    System.out.println("Podaj ilość elementów macierzy:");
+    int ile = in.nextInt();
+    System.out.println("Podaj elementy macierzy:");
+    int[] szerokosc = new int[ile];
+    for(int i=0;i<ile;i++){
+      szerokosc[i] = in.nextInt();
+    }
+    int suma=0;
+    for(int j=0;j<ile;j++){
+      suma=suma+szerokosc[j];
+    }
+    System.out.println(suma);
   }
 }
